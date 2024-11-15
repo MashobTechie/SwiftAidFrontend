6e0f90ee5507c7f38c10b75646288bbd3bbe2bbc
MashobTechie <sheriffdeenmakinde@gmail.com>
2024-11-15 09:22:55 +0000

    Tailwind error solved. SwiftAid is live

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
-    <Router future={{ v7_startTransition: true }}>
+    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
       </Routes>
